adb0697c12814807796142062ccba36a62602863
pranavpk-672 <pranavpk672@gmail.com>
2024-02-01 21:44:51 +0530

    completed

--- googlecloneproj/search/views.py
+++ googlecloneproj/search/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup as bs
+
 
 # Create your views here.
 
@@ -7,4 +10,36 @@ def home(request):
 
 
 def search(request):
-    return render(request,'search.html')
+    if request.method == "POST":
+        search = request.POST[ 'search' ]
+        url = 'https://www.ask.com/web?q='+search
+        res = requests.get(url)
+        soup = bs(res.text, 'lxml')
+
+
+
+        result_listings = soup.find_all('div', {'class': 'PartialSearchResults-item'})
+
+        final_result = []
+        for result in result_listings:
+            result_title = result.find(class_='PartialSearchResults-item-title').text
+            result_url = result.find('a').get('href')
+            result_desc = result.find(class_='PartialSearchResults-item-abstract').text
+
+            final_result.append((result_title,result_url,result_desc))
+        context = {
+            "final_result": final_result
+        }
+
+        return render(request, 'search.html',context)    
+    else:
+
+    
+        return render(request,'search.html')
+
+
+
+
+
+
+
